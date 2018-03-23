adf63fbc77713b42b3a8d6c11b975be9a2d9ff69
vasundharachimmad <vasudha.c8@gmail.com>
2018-03-23 16:50:05 -0700

    Added: security config
    Added: security config

---
+++ src/main/java/security/SecurityConfig.java
@@ -0,0 +1,22 @@
+package security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+  @Autowired
+  public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception
+  {
+    auth
+        .inMemoryAuthentication()
+        .withUser("authorized_user").password("user_password").roles("USER");
+  }
+
+}
